e5f969d6b897a8ec51d4bbdd088d45f6c3216571
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-06-22 16:32:54 +0200

    Drew my first line :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,44 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
+//import jdk.javadoc.internal.doclets.formats.html.markup.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) throws Exception {
         //Label label = new Label("JavaFX is working");
 
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        Stage stage = new Stage();
+
+        Text text = new Text();
+        text.setText("Hello World!");
+        text.setY(50);
+        text.setX(50);
+        text.setFont(Font.font("Arial", FontWeight.BOLD, 50));
+        text.setFill(Color.GREEN);
+
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(200);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.GREEN);
 
         Image icon = new Image(getClass().getResourceAsStream("/images/crossIcon.png"));
         stage.getIcons().add(icon);
@@ -23,6 +47,10 @@ public class Main extends Application {
         stage.setHeight(420);
         stage.setWidth(420);
         stage.setFullScreen(false);
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        stage.setScene(scene);
         stage.show();
     }
 
